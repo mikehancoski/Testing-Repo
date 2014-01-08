3977cfd056a27dd00890e829cf57d1123eda1e7f
kennethwestervelt <kenneth.westervelt@gmail.com>
2014-01-07 23:14:50 -0700

    fake shopping list

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+
+<?php
+
+$testList = array("milk", "eggs", "bread", 
+                  "BFG-9000", "scrap metal", "0.4M sulfuric acid");
+
+$l = count($testList);
+
+// echo $l;
+
+ echo "<h2>Shopping List</h2><ol>";
+
+for ($i=0; $i<$l; $i++) {
+	echo "<li> " . $testList[$i];
+}
+?>
+
+</html>
